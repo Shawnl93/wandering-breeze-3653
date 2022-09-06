e924c5853bb026b5da5023e7d17c0fd046b3a0c3
Shawn Lee <Shawnl93@hotmail.com>
2022-09-06 01:07:10 -0600

    created MVC for contestant

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -2,13 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'the projects show page' do
   before :each do
-    recycled_material_challenge = Challenge.create!(theme: "Recycled Material", project_budget: 1000)
-    furniture_challenge = Challenge.create!(theme: "Apartment Furnishings", project_budget: 1000)
+    @recycled_material_challenge = Challenge.create!(theme: "Recycled Material", project_budget: 1000)
+    @project = @recycled_material_challenge.projects.create!(name: "News Chic", material: "Newspaper")
 
-    @news_chic = recycled_material_challenge.projects.create!(name: "News Chic", material: "Newspaper")
-
+    visit "/projects/#{@project.id}"
   end
   it 'displays the projects name and material' do
-    visit "/projects/#{@news_chic.id}"
+    expect(page).to have_content("News Chic")
+    expect(page).to have_content("Newspaper")
+  end
+
+  it "see theme of the challenge projects belongs to" do
+
   end
 end
